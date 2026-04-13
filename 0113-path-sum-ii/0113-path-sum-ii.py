26581caf666dde550f6fdc31bdc5066eb24ceb17
Shifra <shile6252@gmail.com>
2026-04-13 22:51:58 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -7,14 +7,11 @@
 class Solution:
     def find_path(self, res, one_res, node, targetSum):
         one_res.append(node.val)
-        if node.left is None and node.right is None and targetSum - node.val != 0:
+        if node.left is None and node.right is None:
+            if  targetSum - node.val == 0:
+                res.append(one_res.copy())
             one_res.pop()
             return
-        if targetSum - node.val == 0:
-            if node.left is None and node.right is None:
-                res.append(one_res.copy())
-                one_res.pop()
-                return
         if node.left is not None:
             self.find_path(res, one_res, node.left, targetSum - node.val)
         if node.right is not None:
