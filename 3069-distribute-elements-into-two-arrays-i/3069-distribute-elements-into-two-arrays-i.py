8340bcccae7dfc15e2ce1f2f10a15fb22e2e22cf
Shifra <shile6252@gmail.com>
2026-08-20 15:19:33 +0300

    3069-distribute-elements-into-two-arrays-i Array Easy .py

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def resultArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+        i = 2
+
+        while i < n:
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+            i += 1
+        
+        return arr1 + arr2
