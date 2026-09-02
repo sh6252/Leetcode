e7623d58ab04157a97da0a0455af9bfe09aa28f6
Shifra <shile6252@gmail.com>
2026-09-02 14:26:18 +0300

    3875-construct-uniform-parity-array-i Array Easy .py

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,13 +1,3 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         return True
-        n = len(nums1)
-        odd = 0
-        even = 0
-
-        for num in nums1:
-            odd += num % 2
-            even += (1 - num % 2)
-        
-        return odd == 0 or even == 0 or odd >= 2 or even >= 1
-        
